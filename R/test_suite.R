782d081da342a9dc8bdffcbc23f40a37846ecd99
Jean-Luc Parker <jldegp@gmail.com>
2024-09-28 20:21:31 +0100

    Ideas for a test suite using Quarto

---
+++ R/test_suite.R
@@ -0,0 +1,12 @@
+url <- "https://github.com/Jean-Luc314/Finances.jl.git"
+local_path <- r"{C:\Users\Jean-Luc\Personal\Coding\test_Finances}"
+sha <- "f71cdeac42b49361d8db85cc85739976b092ea7b"
+
+# git2r::clone(url, local_path)
+# repo <- git2r::repository(local_path)
+# git2r::checkout(repo, sha)
+# git2r::sha(git2r::last_commit(repo))
+# quarto::quarto_render(input = "...",
+#                       execute_params = list(...),
+#                       # Or, use file of YAML metadata
+#                       metadata_file = "...")
